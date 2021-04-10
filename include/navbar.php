a4aebc2cec5afce606131a5fb21b50f1dd04d9d1
luis Ramos <techbuilddreams@gmai.com>
2021-04-10 12:03:48 -0400

    created nav bar

---
+++ include/navbar.php
@@ -0,0 +1,19 @@
+<head>
+    <link rel="stylesheet" href="css/navbar.css">
+    <script src="js/navbar.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>D & D Motors</title>
+</head>
+
+<div class="topnav" id="myTopnav">
+  <a href="#home" class="active">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+      <div class="brand">
+        <a href="index.php"><span>D & D Motors</span></a>
+    </div>
+</div>
